aaa04b8edeada3aa65353007fa912d737d3e094a
Paul Bellamy <paul@stellar.org>
2023-01-18 21:29:23 +0000

    Colorful voting indicator

--- components/molecules/voter-info/index.tsx
+++ components/molecules/voter-info/index.tsx
@@ -7,7 +7,7 @@ export interface IVoterInfoProps {
 export function VoterInfo(props: IVoterInfoProps) {
   return (
     <div className="flex flex-col justify-center align-center items-center p-3 w-full space-y-3">
-      <p className="font-semibold">You're {props.isEligible ? "eligible" : "not eligible"} to vote.</p>
+      <p className={`font-semibold ${props.isEligible ? "text-success" : "text-error"}`}>You're {props.isEligible ? "eligible" : "not eligible"} to vote.</p>
       <p>Vote before 2/20</p>
     </div>
   )
