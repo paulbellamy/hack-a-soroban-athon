818780f6a627ecb4d830b9dae99acc57fe02f5e4
Paul Bellamy <paul@stellar.org>
2023-01-18 10:13:53 +0000

    Started styling index with tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,32 +2,35 @@ import React from 'react'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import { Campaign, Pledge } from '../components/organisms'
 import { WalletData } from '../components/molecules'
+import { Button } from '../components/atoms'
 
 const Home: NextPage = () => {
   return (
     <>
       <Head>
         <title>
-          Crowdfund Template - An example of how to run a crowdfund campaign on
-          Soroban.
+          Sorobounty DAO - Propose and vote what you'd like to see built on
+          Soroban smart contracts.
         </title>
         <meta
           name="description"
-          content="An example of loading information from a soroban smart contract"
+          content="Propose and vote what you'd like to see built on Soroban smart contracts"
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <header className={styles.header}>
-        <h3>Starfund</h3>
-        <WalletData />
-      </header>
-      <main className={styles.main}>
-        <div className={styles.content}>
-          <Campaign />
-          <Pledge />
+      <main className="md:container p-9">
+        <div className="space-y-1 w-1/2 min-w-fit">
+          <h1 className="text-3xl"><span className="text-purple">Sorobounty</span> <span className="font-bold">DAO</span></h1>
+          <h3 className="text-sm">v1.20 | Powered by Soroban | Built on Stellar</h3>
+          <p>Propose and vote what you'd like to see built on Soroban smart contracts.</p>
+          <p className="text-purple">Get Started</p>
+          <WalletData />
+          <hr className="border-1 border-divider" />
+          <a href="https://quest.stellar.org/live" className="inline-block border border-button-primary rounded-full bg-button-secondary text-button-primary px-4 py-3">
+            Learn about Stellar Quest
+          </a>
         </div>
       </main>
     </>
