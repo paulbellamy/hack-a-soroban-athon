e68d9f878b9b7ff65b57ea65862532ec1c2db77a
Paul Bellamy <paul@stellar.org>
2023-01-19 09:20:57 +0000

    Fix up the initialize.sh script

--- initialize.sh
+++ initialize.sh
@@ -7,7 +7,8 @@ set -e
 # TODO: Have a nicer way to build Identifiers on the CLI
 export TOKEN_CODE="MYNFT"
 export TOKEN_ADMIN="GBHCFFI5ZCRNHW6RZVR5WG7ERSP7NOOVKF36QBB4PVESDCMZQL6OQYEM"
-TOKEN_ADMIN_HEX="42a4acb6ac6fcdb201384cb1c370a2cdaf14a3bf7f10d7a752366fd77a808163"
+TOKEN_ADMIN_HEX="4e22951dc8a2d3dbd1cd63db1be48c9ff6b9d55177e8043c7d4921899982fce8"
+TOKEN_ADMIN_SECRET="SC4OVVO2RKSUXWAPRMOQSHDZL2Q5COBGOFZSF5MWVTYXC2LHVFFSAVEJ"
 
 case "$1" in
 standalone)
@@ -15,17 +16,17 @@ standalone)
   export SOROBAN_RPC_HOST="http://localhost:8000"
   export SOROBAN_RPC_URL="$SOROBAN_RPC_HOST/soroban/rpc"
   export SOROBAN_NETWORK_PASSPHRASE="Standalone Network ; February 2017"
-  export SOROBAN_SECRET_KEY="SAKCFFFNCE7XAWYMYVRZQYKUK6KMUCDIINLWISJYTMYJLNR2QLCDLFVT"
+  export SOROBAN_SECRET_KEY="$TOKEN_ADMIN_SECRET"
 
   echo Fund token admin account from friendbot
-  # curl "$SOROBAN_RPC_HOST/friendbot?addr=$TOKEN_ADMIN"
+  curl "$SOROBAN_RPC_HOST/friendbot?addr=$TOKEN_ADMIN"
   ;;
 futurenet)
   echo "Using Futurenet network"
-  export SOROBAN_RPC_HOST="https://horizon-futurenet.stellar.org"
+  export SOROBAN_RPC_HOST="http://localhost:8000"
   export SOROBAN_RPC_URL="$SOROBAN_RPC_HOST/soroban/rpc"
   export SOROBAN_NETWORK_PASSPHRASE="Test SDF Future Network ; October 2022"
-  export SOROBAN_SECRET_KEY="SAKCFFFNCE7XAWYMYVRZQYKUK6KMUCDIINLWISJYTMYJLNR2QLCDLFVT"
+  export SOROBAN_SECRET_KEY="$TOKEN_ADMIN_SECRET"
   # TODO: Use friendbot to fund the token admin, or figure our token admin here...
   curl "https://friendbot-futurenet.stellar.org/?addr=$TOKEN_ADMIN"
   ;;
@@ -41,7 +42,8 @@ esac
 
 echo Wrap the Stellar asset for the quest badges to ensure it is accessible from soroban
 mkdir -p .soroban
-TOKEN_ID=$(soroban token wrap --asset "$TOKEN_CODE:$TOKEN_ADMIN")
+# TOKEN_ID=$(soroban token wrap --asset "$TOKEN_CODE:$TOKEN_ADMIN")
+TOKEN_ID=$(soroban token wrap --asset "native")
 echo -n "$TOKEN_ID" > .soroban/token_id
 
 echo Build the voting contract
@@ -50,10 +52,7 @@ make build
 echo Deploy the voting contract
 VOTING_ID="$(
   soroban deploy \
-    --wasm target/wasm32-unknown-unknown/release/soroban_voting_contract.wasm \
-    --network-passphrase "$SOROBAN_NETWORK_PASSPHRASE" \
-    --rpc-url $SOROBAN_RPC_URL \
-    --secret-key $SOROBAN_SECRET_KEY
+    --wasm target/wasm32-unknown-unknown/release/soroban_voting_contract.wasm
 )"
 echo "$VOTING_ID" > .soroban/voting_id
 
@@ -66,8 +65,6 @@ soroban invoke \
   --arg "{\"object\":{\"vec\":[{\"symbol\":\"Account\"},{\"object\":{\"account_id\":{\"public_key_type_ed25519\":\"$TOKEN_ADMIN_HEX\"}}}]}}" \
   --arg "$TOKEN_ID" \
   --arg "1" \
-  --network-passphrase "$SOROBAN_NETWORK_PASSPHRASE" \
-  --rpc-url $SOROBAN_RPC_URL \
-  --secret-key $SOROBAN_SECRET_KEY
+  --wasm target/wasm32-unknown-unknown/release/soroban_voting_contract.wasm
 
 echo "Done"
--- setStatus.sh
+++ setStatus.sh
@@ -5,7 +5,10 @@ set -e
 # TODO: Set the recipient to something reasonable here. Probably whatever account
 # soroban is running stuff as?
 # TODO: Have a nicer way to build Identifiers on the CLI
-TOKEN_ADMIN="GDT2NORMZF6S2T4PT4OBJJ43OPD3GPRNTJG3WVVFB356TUHWZQMU6C3U"
+export TOKEN_CODE="MYNFT"
+export TOKEN_ADMIN="GBHCFFI5ZCRNHW6RZVR5WG7ERSP7NOOVKF36QBB4PVESDCMZQL6OQYEM"
+TOKEN_ADMIN_HEX="4e22951dc8a2d3dbd1cd63db1be48c9ff6b9d55177e8043c7d4921899982fce8"
+TOKEN_ADMIN_SECRET="SC4OVVO2RKSUXWAPRMOQSHDZL2Q5COBGOFZSF5MWVTYXC2LHVFFSAVEJ"
 
 case "$1" in
 standalone)
@@ -13,21 +16,18 @@ standalone)
   export SOROBAN_RPC_HOST="http://localhost:8000"
   export SOROBAN_RPC_URL="$SOROBAN_RPC_HOST/soroban/rpc"
   export SOROBAN_NETWORK_PASSPHRASE="Standalone Network ; February 2017"
-  export SOROBAN_SECRET_KEY="SAKCFFFNCE7XAWYMYVRZQYKUK6KMUCDIINLWISJYTMYJLNR2QLCDLFVT"
+  export SOROBAN_SECRET_KEY="$TOKEN_ADMIN_SECRET"
 
-  echo Fund token admin account from friendbot
-  curl "$SOROBAN_RPC_HOST/friendbot?addr=$TOKEN_ADMIN"
   ;;
 futurenet)
   echo "Using Futurenet network"
   export SOROBAN_RPC_HOST="http://localhost:8000"
   export SOROBAN_RPC_URL="$SOROBAN_RPC_HOST/soroban/rpc"
   export SOROBAN_NETWORK_PASSPHRASE="Test SDF Future Network ; October 2022"
-  export SOROBAN_SECRET_KEY="SAKCFFFNCE7XAWYMYVRZQYKUK6KMUCDIINLWISJYTMYJLNR2QLCDLFVT"
-  # TODO: Use friendbot to fund the token admin, or figure our token admin here...
-  curl "https://friendbot-futurenet.stellar.org/?addr=$TOKEN_ADMIN"
+  export SOROBAN_SECRET_KEY="$TOKEN_ADMIN_SECRET"
   ;;
 sandbox)
+  export SOROBAN_ACCOUNT="$TOKEN_ADMIN"
   # no-op
   ;;
 *)
@@ -54,7 +54,6 @@ esac
 
 soroban invoke \
   --id $(cat .soroban/voting_id) \
-  --account "$TOKEN_ADMIN" \
   --fn set_status \
   --arg $STATUS
 
